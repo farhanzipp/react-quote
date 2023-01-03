a3af6b4030c74e00df40fc522d68ec60afed6e8a
farhanzip <farhanaji317@gmail.com>
2023-01-03 10:04:30 +0700

    add attribution on footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
         <footer className="fixed-bottom "  >
             <Container fluid className="d-flex bg-dark text-muted justify-content-center">
-                <p>made with <AiFillHeart /> by farhanzip</p>
+                <p>made with <AiFillHeart /> by <a href="https://github.com/farhanzipp" target="_blank">farhanzip</a> powered by <a href="https://animechan.vercel.app/" target="blank">Animechan</a></p>
             </Container>
         </footer>
     )
