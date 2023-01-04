5fdbbc2f4e9b2510886a8b941adbff40de99a50a
farhanzip <farhanaji317@gmail.com>
2023-01-04 08:27:02 +0700

    fix footer when in sm size

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,20 +1,19 @@
-import { Container, Nav, Navbar } from "react-bootstrap"
-import { NavItem } from "react-bootstrap/NavItem"
+import { Container, Nav, Navbar} from "react-bootstrap"
 import { AiFillHeart } from "react-icons/ai"
 
 const Footer = () => {
+
     return (
         <Navbar bg="dark" variant="dark" fixed="bottom">
             <Container className="justify-content-center">
                 <Nav >
-                    <Navbar.Text>Made with <AiFillHeart /> by</Navbar.Text>
+                    <Navbar.Text>Made with <AiFillHeart /> by </Navbar.Text>
                     <Nav.Link href="https://github.com/farhanzipp" target="blank">farhanzipp</Nav.Link>
-                    <Navbar.Text>Powered by</Navbar.Text>
-                    <Nav.Link href="https://animechan.vercel.app/" target="_blank">animechan</Nav.Link>
+                    <Navbar.Text className="d-none d-sm-block">Powered by</Navbar.Text>
+                    <Nav.Link className="d-none d-sm-block" href="https://animechan.vercel.app/" target="_blank">animechan</Nav.Link>
                 </Nav>
             </Container>
         </Navbar>
-        
     )     
 }
 
